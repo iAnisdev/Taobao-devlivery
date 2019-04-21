540e8387787de3316fd9699925a2bc7080b9d2b5
Anis <marahmanjs@gmail.com>
2019-04-21 22:41:51 +0500

    get me

--- middleware/verifyOpenId.js
+++ middleware/verifyOpenId.js
@@ -17,3 +17,5 @@ const verifyOpenId = (req , res , next) => {
         })
     }
 }
+
+module.exports = verifyOpenId
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var verifyToken = require('../middleware/verifyToken')
+var verifyOpenId = require('../middleware/verifyOpenId')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* Get ME */
+router.get('/me' , verifyToken ,  (req , res, next) => {
+  res.json(req.user)
+})
 
 module.exports = router;
