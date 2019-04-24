1763c49f6049f5fd177b7627ffc8cf918c94367e
Anis <marahmanjs@gmail.com>
2019-04-24 22:11:58 +0500

    register user

--- routes/users.js
+++ routes/users.js
@@ -20,7 +20,6 @@ router.get('/login', (req, res, next) => {
     let code = req.body.jsCode
     let appid = 'wx7eb82154835d809d'
     let appKey = 'df362811ad98574e8ea39b1e55032389'
-
     axios.get(`https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${appKey}&js_code=${code}&grant_type=authorization_code`).then((resp) => {
       console.log(resp.data)
       let openid = resp.data.openid
@@ -57,4 +56,85 @@ router.get('/login', (req, res, next) => {
     })
   }
 })
+
+router.post('/register' , (req , res , next ) => {
+  if(!req.body.jsCode || !req.body.wxObj || !req.body.phone){
+    res.json({
+      status: 1101,
+      message: 'Invalid JS code'
+    })
+  }else{
+    let code = req.body.jsCode
+    let wxObj = req.body.wxObj
+    let appid = 'wx7eb82154835d809d'
+    let appKey = 'df362811ad98574e8ea39b1e55032389'
+    axios.get(`https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${appKey}&js_code=${code}&grant_type=authorization_code`).then((resp) => {
+      console.log(resp.data)
+      let openid = resp.data.openid
+      if (openid) {
+        userCollection.findOne({ openid: openid }, function (err, user) {
+          if (err) {
+            res.json({
+              status: 1011,
+              message: 'Unkwon Error',
+              err
+            })
+          } else {
+            if (!user) {
+              userCollection.findOne({}, {}, { sort: { id: -1 } }, function (err, record) {
+                let id;
+                if(record){
+                  id= record.id + 1;
+                }else{
+                  id = 1
+                }
+                let userObj = {
+                  lname: wxObj.nickName,
+                  avatarUrl: wxObj.avatarUrl,
+                  id: id,
+                  openid: openid,
+                  phone: req.body.phone,
+                  password: req.body.phone,
+                  gender: wxObj.gender,
+                  country: wxObj.country,
+                  province: wxObj.province,
+                  city: wxObj.city
+                }
+                let newUser = new userCollection(userObj)
+
+              })
+            } else {
+              res.json({
+                status: 1013,
+                message: 'Already Registered',
+                data: user
+              })
+            }
+          }
+        })
+      } else {
+        res.json({
+            status: 1122,
+            data: resp.data
+          })
+      }
+    })
+  }
+})
+
+router.get('/cart' , (req , res , next) => {
+
+})
+
+router.post('/cart/update' , (req , res , next) => {
+  
+})
+
+router.get('/address' , verifyToken , (req , res , next) => {
+
+})
+router.post('/Address/add' , verifyToken , (req , res , next) => {
+
+})
+
 module.exports = router;
